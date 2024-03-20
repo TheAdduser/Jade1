bda2a0df0b3f7f222c33ad0c752b78c98902f79f
TheAdduser <EasyRider190@gmail.com>
2024-03-20 12:13:59 +0100

    Add universal onthology reciver

--- jade1/src/ServiceAgent.java
+++ jade1/src/ServiceAgent.java
@@ -14,30 +14,24 @@ public class ServiceAgent extends Agent {
 		DFAgentDescription dfad = new DFAgentDescription();
 		dfad.setName(getAID());
 		//service no 1
+
+		//service no 2
 		ServiceDescription sd1 = new ServiceDescription();
 		sd1.setType("answers");
-		sd1.setName("wordnet");
-		//service no 2
-		ServiceDescription sd2 = new ServiceDescription();
-		sd2.setType("answers");
-		sd2.setName("dictionary");
+		sd1.setName("dictionary");
 		//service no 3
-		ServiceDescription sd3 = new ServiceDescription();
-		sd3.setType("answers");
-		sd3.setName("esperanto");
+
 		//add them all
+
 		dfad.addServices(sd1);
-		dfad.addServices(sd2);
-		dfad.addServices(sd3);
+
 		try {
 			DFService.register(this,dfad);
 		} catch (FIPAException ex) {
 			ex.printStackTrace();
 		}
-		
-		addBehaviour(new WordnetCyclicBehaviour(this));
+
 		addBehaviour(new DictionaryCyclicBehaviour(this));
-		addBehaviour(new EsperantoCyclicBehaviour(this));
 		//doDelete();
 	}
 	protected void takeDown() {
@@ -87,78 +81,6 @@ public class ServiceAgent extends Agent {
 	}
 }
 
-class WordnetCyclicBehaviour extends CyclicBehaviour
-{
-	ServiceAgent agent;
-	public WordnetCyclicBehaviour(ServiceAgent agent)
-	{
-		this.agent = agent;
-	}
-	public void action()
-	{
-		MessageTemplate template = MessageTemplate.MatchOntology("wordnet");
-		ACLMessage message = agent.receive(template);
-		if (message == null)
-		{
-			block();
-		}
-		else
-		{
-			//process the incoming message
-			String content = message.getContent();
-			ACLMessage reply = message.createReply();
-			reply.setPerformative(ACLMessage.INFORM);
-			String response = "";
-			try
-			{
-				response = agent.makeRequest("wn",content);
-			}
-			catch (NumberFormatException ex)
-			{
-				response = ex.getMessage();
-			}
-			reply.setContent(response);
-			agent.send(reply);
-		}
-	}
-}
-
-class EsperantoCyclicBehaviour extends CyclicBehaviour
-{
-	ServiceAgent agent;
-	public EsperantoCyclicBehaviour(ServiceAgent agent)
-	{
-		this.agent = agent;
-	}
-	public void action()
-	{
-		MessageTemplate template = MessageTemplate.MatchOntology("esperanto");
-		ACLMessage message = agent.receive(template);
-		if (message == null)
-		{
-			block();
-		}
-		else
-		{
-			//process the incoming message
-			String content = message.getContent();
-			ACLMessage reply = message.createReply();
-			reply.setPerformative(ACLMessage.INFORM);
-			String response = "";
-			try
-			{
-				response = agent.makeRequest("fd-epo-eng",content);
-			}
-			catch (NumberFormatException ex)
-			{
-				response = ex.getMessage();
-			}
-			reply.setContent(response);
-			agent.send(reply);
-		}
-	}
-}
-
 class DictionaryCyclicBehaviour extends CyclicBehaviour
 {
 	ServiceAgent agent;
@@ -168,8 +90,7 @@ class DictionaryCyclicBehaviour extends CyclicBehaviour
 	}
 	public void action()
 	{
-		MessageTemplate template = MessageTemplate.MatchOntology("dictionary");
-		ACLMessage message = agent.receive(template);
+		ACLMessage message = agent.receive();
 		if (message == null)
 		{
 			block();
@@ -183,7 +104,7 @@ class DictionaryCyclicBehaviour extends CyclicBehaviour
 			String response = "";
 			try
 			{
-				response = agent.makeRequest("english", content);
+				response = agent.makeRequest(message.getOntology(), content);
 			}
 			catch (NumberFormatException ex)
 			{
